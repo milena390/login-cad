6b750c6e489f0516cebcd77a90af4b27ba90b234
Milena390 <milena.silva390@etec.sp.gov.br>
2025-06-17 15:06:08 -0300

    ok

---
+++ sistema-login-oo/public/cad.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form action="process_login.php" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <label>Nome:</label>
+        <input type="nome" name="nome" required>
+        <label>Senha:</label>
+        <input type="password" name="senha" required>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
